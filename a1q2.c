fee1eb411d23e6d87b651c706a0128596f7e4ae3
Jill <jillian5521@gmail.com>
2022-09-22 20:43:22 -0600

    completed a1q2: work with expressions in C

---
+++ a1q2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <err.h>
+#include <math.h>
+
+int main(){
+
+    int MonsterLvl, HeroLvl;
+    
+    printf("What is the monster's level? ");
+    if (scanf("%d", &MonsterLvl) != 1){
+        errx(1, "Input error, please enter an integer. "); 
+    }
+    printf("What is the hero's level? ");
+    if (scanf("%d", &HeroLvl) != 1){
+        errx(1, "Input error, please enter an integer. ");
+    }
+
+    double xpBase = 100 + (2.5 * MonsterLvl);
+    double LvlCalc = MonsterLvl - HeroLvl;
+    double xpAdvantage = xpBase * pow(1.2, LvlCalc);
+    
+    printf("The monster is level is %d.\nThe hero is level %d.", MonsterLvl, HeroLvl);
+    printf("\nThe monster's base XP value is %.0lf.", xpBase);
+    printf("\nThe monster's adjusted XP is %.0lf.", xpAdvantage);
+
+}
